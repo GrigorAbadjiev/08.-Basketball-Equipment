31f8444cf6ad6122b2472c1645068bd42c4568da
nanji <nanji@DESKTOP-4A8MIDK>
2022-10-30 13:07:53 +0200

    Add project files.

---
+++ 08. Basketball Equipment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         // input
+         int priceForWholeYear = int.Parse(Console.ReadLine());
+
+            // calculate
+            double shosePrice = priceForWholeYear - (priceForWholeYear * 0.4);
+            double jerseyPrice = shosePrice - (shosePrice * 0.2);
+            double ballPrice = jerseyPrice / 4.0;
+            double accesoriesPrice = ballPrice / 5.0;
+
+         // output
+         Console.WriteLine(shosePrice + jerseyPrice + ballPrice + accesoriesPrice + priceForWholeYear); 
+        }
+    }
+}
